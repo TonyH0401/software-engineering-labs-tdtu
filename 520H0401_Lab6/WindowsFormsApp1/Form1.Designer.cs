56b816e1f6ea9a2ca4895d0879460193aecd9f2a
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2022-10-23 13:18:10 +0700

    Add lab06

---
+++ 520H0401_Lab6/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblBirth = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.txtbxEmail = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.rdbtnMale = new System.Windows.Forms.RadioButton();
+            this.rdbtnFemale = new System.Windows.Forms.RadioButton();
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(27, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(27, 77);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 1;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblBirth
+            // 
+            this.lblBirth.AutoSize = true;
+            this.lblBirth.Location = new System.Drawing.Point(27, 162);
+            this.lblBirth.Name = "lblBirth";
+            this.lblBirth.Size = new System.Drawing.Size(28, 13);
+            this.lblBirth.TabIndex = 3;
+            this.lblBirth.Text = "Birth";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(27, 115);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(80, 37);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(209, 20);
+            this.txtbxName.TabIndex = 4;
+            // 
+            // txtbxEmail
+            // 
+            this.txtbxEmail.Location = new System.Drawing.Point(80, 115);
+            this.txtbxEmail.Name = "txtbxEmail";
+            this.txtbxEmail.Size = new System.Drawing.Size(209, 20);
+            this.txtbxEmail.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(80, 162);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(209, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // rdbtnMale
+            // 
+            this.rdbtnMale.AutoSize = true;
+            this.rdbtnMale.Location = new System.Drawing.Point(80, 77);
+            this.rdbtnMale.Name = "rdbtnMale";
+            this.rdbtnMale.Size = new System.Drawing.Size(48, 17);
+            this.rdbtnMale.TabIndex = 7;
+            this.rdbtnMale.TabStop = true;
+            this.rdbtnMale.Text = "Male";
+            this.rdbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnFemale
+            // 
+            this.rdbtnFemale.AutoSize = true;
+            this.rdbtnFemale.Location = new System.Drawing.Point(230, 77);
+            this.rdbtnFemale.Name = "rdbtnFemale";
+            this.rdbtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdbtnFemale.TabIndex = 8;
+            this.rdbtnFemale.TabStop = true;
+            this.rdbtnFemale.Text = "Female";
+            this.rdbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.AllowUserToAddRows = false;
+            this.dataGridViewStudent.AllowUserToDeleteRows = false;
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(313, 37);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.ReadOnly = true;
+            this.dataGridViewStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewStudent.Size = new System.Drawing.Size(462, 236);
+            this.dataGridViewStudent.TabIndex = 9;
+            this.dataGridViewStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewStudent_CellContentClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(80, 207);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(214, 207);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(80, 250);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(214, 250);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 361);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridViewStudent);
+            this.Controls.Add(this.rdbtnFemale);
+            this.Controls.Add(this.rdbtnMale);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtbxEmail);
+            this.Controls.Add(this.txtbxName);
+            this.Controls.Add(this.lblBirth);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblBirth;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtbxName;
+        private System.Windows.Forms.TextBox txtbxEmail;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton rdbtnMale;
+        private System.Windows.Forms.RadioButton rdbtnFemale;
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ 520H0401_Lab6/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtbxName;
+            RefreshData();
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void RefreshData()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Program.strConn);
+                conn.Open();
+
+                //solution 1
+                //String sSQL = "SELECT * FROM student";
+                //solution 2
+                //String sSQL = "EXEC getAllStudent";
+
+                String sSQL = "getAllStudent";
+                SqlCommand cmd = new SqlCommand(sSQL, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                if(dt.Rows.Count > 0)
+                {
+                    dataGridViewStudent.DataSource = dt;
+                }
+                else
+                {
+                    MessageBox.Show("No Data", "Warning No Data");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void dataGridViewStudent_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnAdd.Enabled = false;
+            btnDelete.Enabled = true;
+            btnUpdate.Enabled = true;
+
+            int index = dataGridViewStudent.SelectedCells[0].RowIndex;
+            if(index < 0 || index >= dataGridViewStudent.RowCount)
+            {
+                return;
+            }
+
+            DataGridViewRow row = dataGridViewStudent.Rows[index];
+
+            String name = row.Cells[1].Value.ToString().Trim();
+            String email = row.Cells[4].Value.ToString().Trim();
+            //MessageBox.Show(row.Cells[3].Value.ToString().Trim());
+            String sex = row.Cells[3].Value.ToString().Trim();
+            String date = row.Cells[2].Value.ToString().Trim();
+
+            txtbxName.Text = name;
+            txtbxEmail.Text = email;
+            if (sex.Equals("True"))
+            {
+                rdbtnMale.Checked = true;
+                rdbtnFemale.Checked = false;
+            } 
+            else
+            {
+                rdbtnMale.Checked = false;
+                rdbtnFemale.Checked = true;
+            }
+            //cmd.Parameters.AddWithValue("@ngaySinh", dateTimePickerStudent.Text);
+            dateTimePicker1.Text = date;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //reset all values
+            txtbxEmail.Clear();
+            txtbxName.Clear();
+            rdbtnMale.Checked = false;
+            rdbtnFemale.Checked = false;
+            dateTimePicker1.Value = DateTime.Today;
+
+            //reset all buttons
+            btnAdd.Enabled = true;
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;
+            this.ActiveControl = txtbxName;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int gioiTinhBitValue = (rdbtnMale.Checked == true) ? 1 : 0;
+            try
+            {
+                SqlConnection conn = new SqlConnection(Program.strConn);
+                conn.Open();
+
+                String sSQL = "INSERT INTO student VALUES (@hoTen, @ngaySinh, @gioiTinh, @email)";
+                SqlCommand cmd = new SqlCommand(sSQL, conn);
+                cmd.Parameters.AddWithValue("@hoTen", txtbxName.Text);
+                cmd.Parameters.AddWithValue("@ngaySinh", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@gioiTinh", gioiTinhBitValue);
+                cmd.Parameters.AddWithValue("@email", txtbxEmail.Text);
+
+                int i = cmd.ExecuteNonQuery();
+                if (i != 0)
+                {
+                    MessageBox.Show("Data Added", "Success Box");
+                }
+                else
+                {
+                    MessageBox.Show("No Data", "Warning No Data");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            RefreshData();
+        }
+        // missing button update and button delete
+    }
+}
